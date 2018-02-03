f204f672f32c6d6a149d35ccfd01fe4150d828b5
Brett Wright <brett@brettmwright.com>
2018-02-03 06:23:23 -0500

    Initial Commit

---
+++ SeenSurfer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeenSurfer
+//
+//  Created by Brett Wright on 2/3/18.
+//  Copyright © 2018 brett wright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
